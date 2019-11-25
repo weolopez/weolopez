56878449c6428e3f1273d52ecdbaed80db5e7b68
Mauricio Lopez <lopemaurus.ibm.com@dw135-199-90-220.dhcpw.sest.att.com>
2019-11-25 11:37:07 -0500

    updated styles

---
+++ src/ui-components/section.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'uisection',
+  template: '<ng-container></ng-container>',
+  styles: ['']
+})
+export class SectionComponent {
+}
